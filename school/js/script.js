5650f242f9730810d96c420f42d166da0ad6dc07
17ubi12 <37681833+17ubi12@users.noreply.github.com>
2018-04-13 17:13:48 +0900

    Add files via upload
    upload

---
+++ school/js/script.js
@@ -0,0 +1,35 @@
+var cnt=0;
+var img=$("#visual>img");
+setInterval(function () {
+  next();
+},5000);
+// $('#logo').on('click',function () {
+//   console.log('a');
+//   next();
+// });
+$('#vtext').css({left:($('#visual').width()-$('#vtext').width())/2,top:($('#visual').height()-$('#vtext').height())/2});
+$(window).resize(function () {
+  $('#vtext').css({left:($('#visual').width()-$('#vtext').width())/2,top:($('#visual').height()-$('#vtext').height())/2});
+})
+function next(){
+  if($("#visual>img").is(":animated"))return false;
+  img.eq(cnt).siblings('img').css({'left':'100%'});
+  img.eq(cnt).animate({left:'-100%'},500,function () {
+    $(this).css({'left':'100%'});
+  });
+  cnt++;
+  if(cnt>=img.length)
+  cnt=0;
+  img.eq(cnt).animate({left:0},500);
+}
+function prev(){
+  if($("#visual>img").is(":animated"))return false;
+  img.siblings().css({'left':'-100%'});
+  img.eq(cnt).animate({left:'100%'},function () {
+    $(this).css({'left':'-100%'});
+  });
+  cnt++;
+  if(cnt<=0)
+  cnt=img.length;
+  img.eq(cnt).animate({left:0});
+}
